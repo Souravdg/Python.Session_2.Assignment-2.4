1c418f3667ee9e2f8d5371d813df8b8e2f3449d7
Souravdg <45494334+Souravdg@users.noreply.github.com>
2018-12-14 11:25:02 -0500

    Add files via upload

---
+++ print string in a specified format.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print("WE, THE PEOPLE OF INDIA, \n\thaving solemnly resolved to constitute India into a SOVEREIGN, ! \n\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC \n\t\t and to secure to all its citizens:")
+
